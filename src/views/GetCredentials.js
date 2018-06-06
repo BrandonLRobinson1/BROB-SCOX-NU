948f6d26a8313a4b6c4e03eca606ea203e7e0bb5
Brandon Robinson <BrandonLRobinson1@gmail.com>
2018-06-06 00:34:40 -0400

    page one css comming together

--- src/views/GetCredentials.js
+++ src/views/GetCredentials.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, CardSection } from '../common';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { Button, CardSection, Card } from '../common';
 
 export default class GetCredentials extends Component {
 
   render () {
+    const { container, section1, section2, section3 } = styles;
+
     return (
-      <CardSection>
-        <Button />
-      </ CardSection>
+      <View style={container}>
+        <View style={section1}>
+         <Text>yo</Text>
+        </View>
+        <View style={section2}>
+         <Text>yo</Text>
+        </View>
+        <View style={section3}>
+        <Card>
+        <CardSection>
+          <Text>
+            Logo
+          </Text>
+        </ CardSection>
+        <CardSection>
+          <Button />
+        </ CardSection>
+        <CardSection>
+          <Button />
+        </ CardSection>
+      </Card>
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    textAlign: 'center',
+  },
+  section1: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  section2: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  section3: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
+});
+
+// <Card>
+//         <CardSection>
+//           <Text>
+//             Logo
+//           </Text>
+//         </ CardSection>
+//         <CardSection>
+//           <Button />
+//         </ CardSection>
+//         <CardSection>
+//           <Button />
+//         </ CardSection>
+//       </Card>
